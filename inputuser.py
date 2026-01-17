1da4d298793a9ca559f9d5f327448aae268e0b31
baby pal <babypal0601@gmail.com>
2026-01-17 15:55:36 +0530

    2commit

--- inputuser.py
+++ inputuser.py
@@ -12,3 +12,42 @@
 # print(a+b)
 # print(a-b)
 # print(a*b)
+from operator import index
+# string
+# s = 'baby'
+# print(s)
+# # print(s.count('b'))
+# print(s.index('b'))
+# table
+# u_input = int(input("enter the number"))
+# print(f"{u_input}*1 = {u_input*1}")
+# print(f"{u_input}*2 = {u_input*2}")
+# print(f"{u_input}*3 = {u_input*3}")
+# print(f"{u_input}*4 = {u_input*4}")
+# print(f"{u_input}*5 = {u_input*5}")
+# print(f"{u_input}*6 = {u_input*6}")
+# print(f"{u_input}*7 = {u_input*7}")
+# calculate the area of circle
+# Radius = int(input("enter number"))
+# a = 3.14*(Radius**2)
+# print(a)
+# Two numbers compare karo (greater / less / equal)
+# a = int(input("enter first number"))
+# b = int(input("enter second number"))
+# if a>b:
+#     print("number is greater than number")
+# elif a<b:
+#     print("number is less than number")
+#
+# else:
+#     print("number is equal")
+
+
+
+
+
+
+
+
+
+
